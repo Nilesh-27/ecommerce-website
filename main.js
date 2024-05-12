2c71240f12a00dc97ae21057e38b9f8de4e1e66e
Dokuparthi Nilesh <110710201+Nilesh-27@users.noreply.github.com>
2024-05-12 21:31:48 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import './style.css'
-
-import products from "./api/products.json";
-
-import { showProductContainer } from './homeProductCards';
-//console.log(products);
-
-showProductContainer(products);
